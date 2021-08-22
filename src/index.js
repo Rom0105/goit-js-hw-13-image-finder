10bfca68fbac1a91eb9653226af7909793abb28e
Rom0105 <savosko.photo@gmail.com>
2021-08-22 23:37:43 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+import { error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import card from './templates/card-image.hbs';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.insertAdjacentHTML('beforeend', card);
+
+error({
+  text: 'No image!',
+  delay: 2000,
+});
