4de368a29e01863e6f3f53b9cfaf374235739f3f
Rom0105 <savosko.photo@gmail.com>
2021-08-23 16:46:04 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@ import './sass/main.scss';
 import { error } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import card from './templates/card-image.hbs';
+import axios from 'axios';
+import debounce from 'debounce';
+// import searchImage from './js/apiService.js';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('.input-form'),
 };
 
-refs.gallery.insertAdjacentHTML('beforeend', card);
+refs.form.addEventListener('input', debounce(pixHandler, 500));
+let currentPage = 1;
+
+function pixHandler(event) {
+  event.preventDefault();
+
+  const value = refs.input.value;
+  axios
+    .get(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${currentPage}&per_page=12&key=23040897-f684e552d269990a649c2a9ea`,
+    )
+    .then(image => markup(image.data.hits))
+    .then(() => (currentPage += 1))
+    .catch(error => console.error(error));
+}
+
+function markup(image) {
+  refs.gallery.insertAdjacentHTML('beforeend', card(image));
+}
 
 error({
   text: 'No image!',
---
+++ src/js/apiService.js
@@ -0,0 +1,16 @@
+// import axios from 'axios';
+
+// const BASE_URL = 'https://pixabay.com/api';
+// let currentPage = 1;
+
+// function searchImage(value) {
+//   return axios
+//     .get(
+//       `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${value}&page=${currentPage}&per_page=12&key=23040897-f684e552d269990a649c2a9ea`,
+//     )
+//     .then(image => image)
+//     .then(() => (currentPage += 1))
+//     .catch(error => console.error(error));
+// }
+
+// export default searchImage;
